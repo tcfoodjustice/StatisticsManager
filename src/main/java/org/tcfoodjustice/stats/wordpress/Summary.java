a0e41cfed29c9d4f827b8d45f1187bf4b059a0e4
andrewlarsen514 <andrew.larse514@gmail.com>
2017-03-27 23:26:17 -0500

    add missed checkin

---
+++ src/main/java/org/tcfoodjustice/stats/wordpress/Summary.java
@@ -0,0 +1,62 @@
+package org.tcfoodjustice.stats.wordpress;
+
+/**
+ * Created by andrew.larsen on 3/27/2017.
+ */
+public class Summary {
+
+    private Integer views;
+    private Integer visitors;
+    private Integer likes;
+    private Integer reblogs;
+    private Integer comments;
+    private Integer followers;
+
+    public Integer getViews() {
+        return views;
+    }
+
+    public void setViews(Integer views) {
+        this.views = views;
+    }
+
+    public Integer getVisitors() {
+        return visitors;
+    }
+
+    public void setVisitors(Integer visitors) {
+        this.visitors = visitors;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getReblogs() {
+        return reblogs;
+    }
+
+    public void setReblogs(Integer reblogs) {
+        this.reblogs = reblogs;
+    }
+
+    public Integer getComments() {
+        return comments;
+    }
+
+    public void setComments(Integer comments) {
+        this.comments = comments;
+    }
+
+    public Integer getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Integer followers) {
+        this.followers = followers;
+    }
+}
